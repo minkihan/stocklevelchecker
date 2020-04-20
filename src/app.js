bf50e418a5efb4b10f262e76b1085f9d2cb800ee
minki han <ayakano5@gmail.com>
2020-04-20 13:53:13 +0900

    ...

--- src/app.js
+++ src/app.js
@@ -4,6 +4,9 @@ const patagonia = require( "./model/patagonia.js" ) ;
 const parse_patagonia = require( "./service/parse_patagonia.js" ) ;
 
 ( async() => {
+    console.log( "welcome to node.js" ) ;
+    console.log( "welcome to node.js" ) ;
+
     //await init.start() ;
     await parse_patagonia.getList() ;
     //await patagonia.find() ;
