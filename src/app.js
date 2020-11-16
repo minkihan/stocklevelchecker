247fc64fead7731c54a3c64f96b9ac8ef4efd3db
minki han <ayakano5@gmail.com>
2020-11-16 22:04:56 +0900

    init.js 주석처리

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-const init = require( "./common/init.js" ) ;
+//const init = require( "./common/init.js" ) ;
 const jav = require( "./model/jav.js" ) ;
 const parse_jav = require( "./service/parse_jav.js" ) ;
 
