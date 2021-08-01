0bbc404c8a3f6a0c7f63782e6a48aba2c82be874
minkihan <ayakano5@gmail.com>
2021-08-01 18:08:24 +0900

    remove script

--- src/__script/3_rclone_move.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# move renamed(3) to complete(4)
-sudo mv /tr/3_renamed/* /tr/4_complete/
-
-# rclone move 
-sudo nohup sudo rclone move /tr/4_complete/ gdupload: --delete-empty-src-dirs > /dev/null 2>&1 &
