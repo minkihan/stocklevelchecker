ed1f3deb4c0eaf873b92df4841999c4adb917784
minkihan <ayakano5@gmail.com>
2021-08-21 16:29:50 +0900

    실행 스크립트 변경 - azure vm

--- script/77_start.sh
+++ script/77_start.sh
@@ -11,14 +11,19 @@ PASS=1
 echo "---------------------------------------------------"
 date
 
-if pgrep -x Xvfb > /dev/null
-then
-    echo "Xvfb is running ..."
-else 
-    echo "process start : Xvfb"
+#if pgrep -x Xvfb > /dev/null
+#then
+#    echo "Xvfb is running ..."
+#else 
     /usr/bin/Xvfb -ac :99 -screen 0 1280x1024x16 2>&1 &
     export DISPLAY=:99
-fi
+    echo "process start : Xvfb"
+#fi
 
 node ~/coda/stocklevelchecker/src/app >> ~/coda/stocklevelchecker/log/jav.$CURRENT_DATE.log
+
+xvfb=$(pgrep Xvfb)
+kill -9 $xvfb
+echo "process stop : Xvfb"
+
 date
